1bef9ad752d0bdb4f3b831e4603862ea73454c61
shahar haviv <shahar.haviv125@gmail.com>
2024-11-07 09:12:42 +0200

    adding num to words

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,17 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		String[] numbers = args[0].split("");
+		switch (numbers.length) {
+			case 1:
+				System.out.println("0 hundreds, 0 tens, and " + numbers[0] + " ones.");
+				break;
+			case 2:
+				System.out.println("0 hundreds, " + numbers[0] + " tens, and " + numbers[1] + " ones.");
+				break;
+			case 3:
+				System.out.println(numbers[0] + " hundreds, " + numbers[1] + " tens, and " + numbers[2] + " ones.");
+				break;
+		}
 	}
 }
