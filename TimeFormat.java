412dae156dd8666f61a87e80222b44269a317c28
shahar haviv <shahar.haviv125@gmail.com>
2024-11-11 11:52:23 +0200

    fixing hours

--- TimeFormat.java
+++ TimeFormat.java
@@ -2,10 +2,9 @@
 public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		String hoursString = ("" + args[0].charAt(0) + args[0].charAt(1));
 		String minutes = ("" + args[0].charAt(3) + args[0].charAt(4));
 		if (hours < 12) {
-			System.out.println(hoursString + ":" + minutes + " AM");
+			System.out.println(hours + ":" + minutes + " AM");
 		}
 		if (hours == 12) {
 			System.out.println(hours + ":" + minutes + " PM");
